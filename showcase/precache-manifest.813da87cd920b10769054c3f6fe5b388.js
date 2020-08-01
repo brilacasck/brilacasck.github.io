6af0e9f0c3e0922707c369fdeb861c1883f1f710
alireza kavian <kay.alireza@gmail.com>
2020-08-01 23:13:40 +0430

    ed

---
+++ showcase/precache-manifest.813da87cd920b10769054c3f6fe5b388.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5099853a741aa91ea1e9823287c791c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f046ee4821802d758f32",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "d177a85e31987c37661c",
+    "url": "/static/css/main.dfedb6bb.chunk.css"
+  },
+  {
+    "revision": "f046ee4821802d758f32",
+    "url": "/static/js/2.fbd75d45.chunk.js"
+  },
+  {
+    "revision": "d177a85e31987c37661c",
+    "url": "/static/js/main.52be3e50.chunk.js"
+  },
+  {
+    "revision": "be96e899b0af4f1c6da5",
+    "url": "/static/js/runtime-main.a7726d00.js"
+  },
+  {
+    "revision": "1b338e4684bebb6e7b3c52365dbebd2b",
+    "url": "/static/media/logo.1b338e46.png"
+  }
+]);
